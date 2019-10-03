42a298777e0629215a586e0e5ff9da798810eae1
crystalDf <chendong333@gmail.com>
2019-10-03 18:22:21 +0800

    Chapter 01 GeoQuiz

---
+++ app/src/main/java/com/star/geoquiz/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.star.geoquiz
+
+import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        true_button.setOnClickListener { view: View ->
+            val toast = Toast.makeText(
+                this,
+                R.string.correct_toast,
+                Toast.LENGTH_SHORT)
+            toast.setGravity(Gravity.TOP, 0, 0)
+            toast.show()
+        }
+
+        false_button.setOnClickListener { view: View ->
+            val toast = Toast.makeText(
+                this,
+                R.string.incorrect_toast,
+                Toast.LENGTH_SHORT)
+            toast.setGravity(Gravity.TOP, 0, 0)
+            toast.show()
+        }
+    }
+}
